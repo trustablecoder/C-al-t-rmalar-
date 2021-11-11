4432bc9e6f78190b8bdd4497880f4a83e26bf707
Simge <56368163+trustablecoder@users.noreply.github.com>
2021-11-11 18:42:20 +0300

    Add files via upload

---
+++ nesne_tabanli_programlama/nesne_tabanli_programlama/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace nesne_tabanli_programlama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMarka1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblModel1 = new System.Windows.Forms.Label();
+            this.lblUretimYili1 = new System.Windows.Forms.Label();
+            this.lblFiyat1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblMarka2 = new System.Windows.Forms.Label();
+            this.lblModel2 = new System.Windows.Forms.Label();
+            this.lblUretimYili2 = new System.Windows.Forms.Label();
+            this.lblFiyat2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Araç bilgilerini getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMarka1
+            // 
+            this.lblMarka1.AutoSize = true;
+            this.lblMarka1.Location = new System.Drawing.Point(112, 105);
+            this.lblMarka1.Name = "lblMarka1";
+            this.lblMarka1.Size = new System.Drawing.Size(69, 17);
+            this.lblMarka1.TabIndex = 1;
+            this.lblMarka1.Text = "lblMarka1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Markası:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(37, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Modeli:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(10, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Üretim Yılı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(45, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fiyatı:";
+            // 
+            // lblModel1
+            // 
+            this.lblModel1.AutoSize = true;
+            this.lblModel1.Location = new System.Drawing.Point(112, 154);
+            this.lblModel1.Name = "lblModel1";
+            this.lblModel1.Size = new System.Drawing.Size(68, 17);
+            this.lblModel1.TabIndex = 6;
+            this.lblModel1.Text = "lblModel1";
+            // 
+            // lblUretimYili1
+            // 
+            this.lblUretimYili1.AutoSize = true;
+            this.lblUretimYili1.Location = new System.Drawing.Point(112, 201);
+            this.lblUretimYili1.Name = "lblUretimYili1";
+            this.lblUretimYili1.Size = new System.Drawing.Size(89, 17);
+            this.lblUretimYili1.TabIndex = 7;
+            this.lblUretimYili1.Text = "lblUretimYili1";
+            // 
+            // lblFiyat1
+            // 
+            this.lblFiyat1.AutoSize = true;
+            this.lblFiyat1.Location = new System.Drawing.Point(112, 247);
+            this.lblFiyat1.Name = "lblFiyat1";
+            this.lblFiyat1.Size = new System.Drawing.Size(60, 17);
+            this.lblFiyat1.TabIndex = 8;
+            this.lblFiyat1.Text = "lblFiyat1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(80, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Araba1";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblFiyat1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblUretimYili1);
+            this.groupBox1.Controls.Add(this.lblMarka1);
+            this.groupBox1.Controls.Add(this.lblModel1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(42, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 317);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblFiyat2);
+            this.groupBox2.Controls.Add(this.lblUretimYili2);
+            this.groupBox2.Controls.Add(this.lblModel2);
+            this.groupBox2.Controls.Add(this.lblMarka2);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(336, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 317);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(24, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Markası:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(33, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Modeli:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(6, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Üretim Yılı:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(41, 247);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 17);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Fiyatı:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(95, 28);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 24);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Araba2";
+            // 
+            // lblMarka2
+            // 
+            this.lblMarka2.AutoSize = true;
+            this.lblMarka2.Location = new System.Drawing.Point(117, 105);
+            this.lblMarka2.Name = "lblMarka2";
+            this.lblMarka2.Size = new System.Drawing.Size(69, 17);
+            this.lblMarka2.TabIndex = 11;
+            this.lblMarka2.Text = "lblMarka2";
+            // 
+            // lblModel2
+            // 
+            this.lblModel2.AutoSize = true;
+            this.lblModel2.Location = new System.Drawing.Point(120, 154);
+            this.lblModel2.Name = "lblModel2";
+            this.lblModel2.Size = new System.Drawing.Size(68, 17);
+            this.lblModel2.TabIndex = 12;
+            this.lblModel2.Text = "lblModel2";
+            // 
+            // lblUretimYili2
+            // 
+            this.lblUretimYili2.AutoSize = true;
+            this.lblUretimYili2.Location = new System.Drawing.Point(117, 201);
+            this.lblUretimYili2.Name = "lblUretimYili2";
+            this.lblUretimYili2.Size = new System.Drawing.Size(89, 17);
+            this.lblUretimYili2.TabIndex = 13;
+            this.lblUretimYili2.Text = "lblUretimYili2";
+            // 
+            // lblFiyat2
+            // 
+            this.lblFiyat2.AutoSize = true;
+            this.lblFiyat2.Location = new System.Drawing.Point(120, 247);
+            this.lblFiyat2.Name = "lblFiyat2";
+            this.lblFiyat2.Size = new System.Drawing.Size(60, 17);
+            this.lblFiyat2.TabIndex = 14;
+            this.lblFiyat2.Text = "lblFiyat2";
+            this.lblFiyat2.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblMarka1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblModel1;
+        private System.Windows.Forms.Label lblUretimYili1;
+        private System.Windows.Forms.Label lblFiyat1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblFiyat2;
+        private System.Windows.Forms.Label lblUretimYili2;
+        private System.Windows.Forms.Label lblModel2;
+        private System.Windows.Forms.Label lblMarka2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ nesne_tabanli_programlama/nesne_tabanli_programlama/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nesne_tabanli_programlama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Araba otomobil1 = new Araba();
+            otomobil1.marka = "BMW";
+            otomobil1.model = "520";
+            otomobil1.uretim_yili = "2020";
+            otomobil1.fiyat = 850000;
+            lblMarka1.Text = otomobil1.marka;
+            lblModel1.Text = otomobil1.model;
+            lblUretimYili1.Text = otomobil1.uretim_yili;
+            lblFiyat1.Text = otomobil1.fiyat.ToString();
+            Araba otomobil2 = new Araba();
+            otomobil2.marka = "Renault";
+            otomobil2.model = "Megane";
+            otomobil2.uretim_yili = "2016";
+            otomobil2.fiyat = 250000;
+            lblMarka2.Text = otomobil2.marka;
+            lblModel2.Text = otomobil2.model;
+            lblUretimYili2.Text = otomobil2.uretim_yili;
+            lblFiyat2.Text = otomobil2.fiyat.ToString();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
